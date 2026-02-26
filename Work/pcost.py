af56b459cfa135e0f4f8b3e83e66a0bcf1aed9c7
SirDeP <biogene.53418@gmail.com>
2026-02-26 09:09:58 +0100

    End of chapter 1

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
 def portfolio_cost(filename):
     totalcost = 0.0
 
     with open(filename, 'rt') as f:
-        headers = next(f)
-        for line in f:
-            row = line.split(',')
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
             try:
                 totalcost += int(row[1]) * float(row[2])
             except:
                 print("multiplication failure")
     return totalcost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
