d91369c6bcc65f833970eb6cea9d5881437a6fa5
SirDeP <biogene.53418@gmail.com>
2026-02-27 10:54:29 +0100

    End of 2.3

--- Work/report.py
+++ Work/report.py
@@ -49,10 +49,27 @@ def gain_loss(f_portfolio, f_pricelist):
         print(stock, f'{bought_value:0.2f}', f'{current_value:0.2f}', f'{gain_los:0.2f}')
     print('Total gain loss', f'{total_gain_los}')
 
-def make_report(f_portfolio, f_):
-    portfolio = read_portfolio('Data/portfolio.csv')
-    prices = read_prices('Data/prices.csv')
-    
+def make_report(portfolio, prices):
+    report = []
+    for i in range(len(portfolio)):
+        stockname = portfolio[i]['name']
+        gainloss = float(portfolio[i]['price']) - prices[stockname]
+        res = (stockname, int(portfolio[i]['shares']), float(prices[ stockname ]), -float(gainloss) ) #float(portfolio[i]['price']) )
+        report.append(res)
+    return report
 
-gain_loss('Data/portfolio.csv', 'Data/prices.csv')
+report =make_report(read_portfolio('Data/portfolio.csv'), read_prices('Data/prices.csv'))
 
+#for r in report:
+#    print( '%10s %10d %10.2f %10.2f' % r )
+size = 10
+headers = ('Name', 'Shares', 'Price', 'Change')
+print ( f'{headers[0]:>{size}s} {headers[1]:>{size}s} {headers[2]:>{size}s} {headers[3]:>{size}s}')
+s = ''
+for i in range(size):
+    s += '-'
+
+print(s, s, s, s)
+for name, shares, price, change in report:
+    price = '$' + str(price)
+    print(f'{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}')
